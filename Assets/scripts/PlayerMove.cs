0f7252982bfa1811089c1e745e88ee9b022f1292
Kevin Weeks <kevin.weeks@comcast.net>
2016-10-13 00:35:13 -0700

    Added in basic (not really working) multiplayer

---
+++ Assets/scripts/PlayerMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    void Start()
+    {
+        transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
+        
+    }
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * 0.1f;
+        var z = Input.GetAxis("Vertical") * 0.1f;
+
+        transform.Translate(x, 0, z);
+    }
+}
